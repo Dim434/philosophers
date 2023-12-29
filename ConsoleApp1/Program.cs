88d8d2bad37fc96780e68f749d9604df5f17c5f0
ddif <ww@ww>
2023-12-29 21:56:52 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace ConsoleApp1;
+
+class Philosopher(int id, Mutex leftFork, Mutex rightFork, bool isLast)
+{
+    public void Dine()
+    {
+        while (true)
+        {
+            Think();
+
+            // Последний философ берет сначала левую вилку, затем правую
+            if (isLast)
+            {
+                leftFork.WaitOne();
+                rightFork.WaitOne();
+            }
+            else
+            {
+                rightFork.WaitOne();
+                leftFork.WaitOne();
+            }
+
+            Eat();
+
+            leftFork.ReleaseMutex();
+            rightFork.ReleaseMutex();
+        }
+    }
+
+    private void Think()
+    {
+        Console.WriteLine($"Philosopher {id} is thinking.");
+        Thread.Sleep(new Random().Next(1000, 2000));
+    }
+
+    private void Eat()
+    {
+        Console.WriteLine($"Philosopher {id} is eating.");
+        Thread.Sleep(new Random().Next(1000, 2000));
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Мьютексы: Каждая вилка представлена мьютексом. Философы должны захватить две вилки, чтобы начать есть.
+        const int numPhilosophers = 100;
+        var forks = new Mutex[numPhilosophers];
+        var philosophers = new Philosopher[numPhilosophers];
+        var threads = new Thread[numPhilosophers];
+
+        for (var i = 0; i < numPhilosophers; i++)
+        {
+            forks[i] = new Mutex();
+        }
+
+        for (var i = 0; i < numPhilosophers; i++)
+        {
+            var leftFork = forks[i];
+            var rightFork = forks[(i + 1) % numPhilosophers];
+            philosophers[i] = new Philosopher(i, leftFork, rightFork, i == numPhilosophers - 1);
+            threads[i] = new Thread(philosophers[i].Dine);
+            threads[i].Start();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace ConsoleApp2;
+
+internal class Philosopher(int id, Semaphore semaphore, Mutex leftFork, Mutex rightFork)
+{
+    public void Dine()
+    {
+        while (true)
+        {
+            Think();
+            semaphore.WaitOne();
+            Eat();
+            semaphore.Release();
+        }
+    }
+
+    private void Think()
+    {
+        Console.WriteLine($"Philosopher {id} is thinking.");
+        Thread.Sleep(RandomTime());
+    }
+
+    private void Eat()
+    {
+        leftFork.WaitOne();
+        rightFork.WaitOne();
+
+        Console.WriteLine($"Philosopher {id} is eating.");
+        Thread.Sleep(RandomTime());
+
+        leftFork.ReleaseMutex();
+        rightFork.ReleaseMutex();
+    }
+
+    private int RandomTime()
+    {
+        return new Random().Next(1000, 2000);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Семафор: Ограничивает количество философов, которые могут одновременно пытаться есть. В этом примере он установлен на numPhilosophers - 1, чтобы всегда была хотя бы одна вилка свободна.
+        // Мьютексы: Аналогично первому, должны захватить чтобы есть
+        const int numPhilosophers = 5;
+        var semaphore = new Semaphore(numPhilosophers - 1, numPhilosophers - 1);
+        var forks = new Mutex[numPhilosophers];
+        var philosophers = new Philosopher[numPhilosophers];
+        var threads = new Thread[numPhilosophers];
+
+        for (int i = 0; i < numPhilosophers; i++)
+        {
+            forks[i] = new Mutex();
+        }
+
+        for (int i = 0; i < numPhilosophers; i++)
+        {
+            var leftFork = forks[i];
+            var rightFork = forks[(i + 1) % numPhilosophers];
+
+            philosophers[i] = new Philosopher(i, semaphore, leftFork, rightFork);
+            threads[i] = new Thread(philosophers[i].Dine);
+            threads[i].Start();
+        }
+    }
+}
